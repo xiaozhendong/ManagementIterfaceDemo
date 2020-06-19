8b39dbbb72eefb7a98084402412e117e0c938800
xiaozhendong <coder_xiao@163.com>
2020-06-19 23:42:51 +0800

    add model content and fix readme.md

---
+++ src/models/counter.ts
@@ -0,0 +1,17 @@
+import {IRootDispatch} from 'ice';
+
+export default {
+  state:{
+
+  },
+  reducer:{
+
+  },
+  effect:(dispatch:IRootDispatch)=>({
+      async decrementAsync(){
+
+      }
+
+  })
+
+}
